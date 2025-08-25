06ddb0f96b3622522d8ca1a0d4a5da679ea7b46b
Surya Singh <singhsurya1012@gmail.com>
2025-08-25 15:55:53 +0200

    Adding Safe Area Provider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,15 @@
 import { Stack } from 'expo-router';
+
+import { ClerkProvider, ClerkLoaded } from '@clerk/clerk-expo';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { SafeAreaProvider, initialWindowMetrics } from 'react-native-safe-area-context';
+import { StatusBar } from 'expo-status-bar';
+import { tokenCache } from '@clerk/clerk-expo/token-cache';
+
 import { ThemeProvider } from '../src/styles/theme/ThemeContext';
 import { ToastProvider } from '../hooks/providers/ToastProvider';
-import { StatusBar } from 'expo-status-bar';
-import { ClerkProvider, ClerkLoaded } from '@clerk/clerk-expo';
-import { tokenCache } from '@clerk/clerk-expo/token-cache';
-import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from '../lib/queryClient';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
 
@@ -18,26 +21,28 @@ if (!publishableKey) {
 
 export default function RootLayout() {
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <ClerkProvider
-        tokenCache={tokenCache}
-        publishableKey={publishableKey}
-      >
-        <QueryClientProvider client={queryClient}>
-          <ThemeProvider>
-            <ToastProvider>
-              <ClerkLoaded>
-                <StatusBar style="auto" />
-                <Stack screenOptions={{ headerShown: false, animation: 'none' }}>
-                  <Stack.Screen name="index" />
-                  <Stack.Screen name="(auth)" />
-                  <Stack.Screen name="(tabs)" />
-                </Stack>
-              </ClerkLoaded>
-            </ToastProvider>
-          </ThemeProvider>
-        </QueryClientProvider>
-      </ClerkProvider>
-    </GestureHandlerRootView>
+    <SafeAreaProvider initialMetrics={initialWindowMetrics}>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <ClerkProvider
+          tokenCache={tokenCache}
+          publishableKey={publishableKey}
+        >
+          <QueryClientProvider client={queryClient}>
+            <ThemeProvider>
+              <ToastProvider>
+                <ClerkLoaded>
+                  <StatusBar style="auto" />
+                  <Stack screenOptions={{ headerShown: false, animation: 'none' }}>
+                    <Stack.Screen name="index" />
+                    <Stack.Screen name="(auth)" />
+                    <Stack.Screen name="(tabs)" />
+                  </Stack>
+                </ClerkLoaded>
+              </ToastProvider>
+            </ThemeProvider>
+          </QueryClientProvider>
+        </ClerkProvider>
+      </GestureHandlerRootView>
+    </SafeAreaProvider>
   );
 }
