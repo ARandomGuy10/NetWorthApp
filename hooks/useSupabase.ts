1e1c76919cd39d6d78500b4ccc897ac7489aaee4
Surya Singh <singhsurya1012@gmail.com>
2025-09-01 23:30:10 +0200

    removing log

--- hooks/useSupabase.ts
+++ hooks/useSupabase.ts
@@ -17,7 +17,7 @@ export const useSupabase = () => {
             const token = await getToken();
             if (token) headers.set('Authorization', `Bearer ${token}`);
           }
-          console.log(headers.get('Authorization'));
+          //console.log(headers.get('Authorization'));
           return fetch(url, {...options, headers});
         },
       },
